c093e778459d7bda511174b52d46486fd8701584
LuuPhong1 <phongb1910278@student.ctu.edu.vn>
2023-02-08 16:57:48 +0700

    Cai dat cac handler truy xuat CSDL

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,9 +1,28 @@
+const { MongoAPIError } = require("mongodb");
+const ContactService = require("../services/contact.service");
+const MongoDB = require("../untils/mongodb.until");
+const ApiError = require("../api-error");
+
 exports.create = (req, res) => {
     res.send({ message: "create handler" });
 };
 
-exports.findAll = (req, res) => {
-    res.send({ message: "findAll handler" });
+exports.findAll = (req, res, next) => {
+    let documents = [];
+    try {
+        const contactService = new ContactService(MongoDB.client);
+        const { name } = req.query;
+        if (name) {
+            documents = await contactService.findByName(name);
+        } else {
+            documents = await contactService.findAll({});
+        }
+    } catch (error) {
+        return next(
+            new ApiError(500, " An error occurred while retrieving contacts")
+        );
+    }
+    return res.send(documents);
 };
 
 exports.findOne = (req, res) => {
@@ -20,4 +39,19 @@ exports.deleteAll = (req, res) => {
 };
 exports.findAllFavorite = (req, res) => {
     res.send({ message: "findAllFavorite handler" });
+};
+
+exports.create = async(req, res, next) => {
+    if (!req.body ? .name) {
+        return next(new ApiError(400, "Name can not be empty"));
+    }
+    try {
+        const contactService = new contactService(MongoDB.client);
+        const document = await contactService.create(req.body);
+        return res.send(document);
+    } catch (error) {
+        return next(
+            new ApiError(500, "An error occurred while creating the contact")
+        );
+    }
 };
